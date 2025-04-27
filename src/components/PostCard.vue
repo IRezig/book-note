cdcc8091e024fd6e4584a5dad465b930d7f11114
irezig <rezig.ibtissam@gmail.com>
2025-04-27 13:06:13 +0200

    add new images

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,17 +1,21 @@
 <template>
-   <Card style="width: 20rem; height: 20rem; overflow: hidden; margin-top: 1rem" @click="handleClick">
+   <Card class="w-80 h-80 overflow-hidden mt-4" @click="handleClick">
         <template #header>
-            <img alt="user header" src="https://primefaces.org/cdn/primevue/images/usercard.png" />
+            <img
+                :alt="'user header'"
+                :src="selectedImage"
+                class="w-full h-40 object-cover cursor-pointer"
+                tabindex="0"
+                aria-label="Post header image"
+            />
         </template>
         <template #title>{{ post.title }}</template>
-
         <template #subtitle>{{ formatToDate(post.created_at) }}</template>
         <template #content>
             <p class="m-0">
               {{ post.content }}
             </p>
         </template>
-        
     </Card>
 </template>
 
@@ -21,9 +25,21 @@ import Card from 'primevue/card';
 import { useRouter } from 'vue-router';
 import { formatToDate } from '../utils/date';
 
-
 const router = useRouter();
 const { post } = defineProps<{ post: Reflection }>();
+
+const imagePaths = [
+  new URL('../../public/code_1.jpg', import.meta.url).href,
+  new URL('../../public/code_2.jpg', import.meta.url).href,
+  new URL('../../public/code_3.jpg', import.meta.url).href
+];
+
+const shuffleImages = (images: string[]): string => {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  return images[randomIndex];
+};
+
+const selectedImage = shuffleImages(imagePaths);
 
 const handleClick = () => {
   router.push(`/reflection/${post.id}`);
